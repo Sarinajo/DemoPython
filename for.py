cbe3566f8f15fd2198e7fc6751f63616fe156c10
Sarina Joshi <joshisarina19@gmail.com>
2025-11-19 14:09:04 +0545

    commit

---
+++ for.py
@@ -0,0 +1,20 @@
+for letter in "Sarina":
+    print(letter)
+
+friends = ["alisa","renusha","sneha"]
+for friend in friends:
+    print(friend)
+
+for index in range(10):
+    print(index)
+
+for indexs in range(3,10):
+    print(indexs)
+
+for ind in range(len(friends)):
+    print(friends[ind])
+
+people = ["alisa","kritigya"]
+for manxe in range(len(people)):
+    print(people[manxe])
+print(people[1])

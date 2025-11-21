f54e1dc3bac3116ac1f06a9148667d46653f49f5
Sarina Joshi <joshisarina19@gmail.com>
2025-11-21 13:02:33 +0545

    madlibs game

---
+++ madlibs.py
@@ -0,0 +1,6 @@
+location=input("enter the location you want: ")
+think = input("what you think about her: ")
+behaviour = input("what behaviour you think: ")
+behaviour2 = input("what second behaviour you think: ")
+madlib = f"her house is in {location}. which makes her kind of {think}. she is {behaviour}, which makes her {behaviour2}."
+print(madlib)
